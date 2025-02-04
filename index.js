b3df4babe625e6a2286e1813e9a97019a868a46d
ashuofficial12 <ashutoshrockwell@gmail.com>
2025-02-04 18:01:20 +0530

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const authRoutes = require("./src/routes/authRoutes");
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:5001", credentials: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 
